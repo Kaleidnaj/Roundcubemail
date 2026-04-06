af284bfd0c62288c38fe5cea706e56852fc7cf13
Kaleidnaj <142833138+Kaleidnaj@users.noreply.github.com>
2026-04-06 09:58:32 -0400

    Update nginxinstall.sh

--- nginx/nginxinstall.sh
+++ nginx/nginxinstall.sh
@@ -1,1 +1,12 @@
+```
+sudo apt get update
+apt install nginx
+```bash
+#Create template directory in nginx folder
+mkdir /etc/nginx/templates
+```
 
+cd /etc/nginx/templates
+#Create these confing files
+touch hsts.tmpl;touch https_redirect.tmpl;touch misc.tmpl;touch php-catchall.tmpl; touch roundcube.tmpl
+#Make sure to store the certificate files in /etc/ssl/certs/ubiquity
